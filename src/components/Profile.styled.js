c82c8594613abd381eb7f53a9ae62e94f870a928
Oksana-Pak <okspak7@gmail.com>
2022-12-14 17:59:19 +0200

    add task 1

---
+++ src/components/Profile.styled.js
@@ -0,0 +1,66 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  background-color: #d5dce6;
+`;
+export const ProfileContainer = styled.div`
+  width: 30%;
+  margin: 0 auto;
+  border-radius: 8px;
+  overflow: hidden;
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+
+  text-align: center;
+`;
+export const Description = styled.div`
+  padding-top: 50px;
+  padding-bottom: 50px;
+  background-color: #fff;
+`;
+export const Avatar = styled.img`
+  width: 40%;
+  display: block;
+  margin: 0 auto;
+  border-radius: 50%;
+  background-color: #d5dce6;
+`;
+export const UserName = styled.p`
+  margin: 40px 0px 20px 0px;
+  font-size: 26px;
+  font-weight: bold;
+`;
+export const Tag = styled.p`
+  margin: 0;
+  margin-bottom: 20px;
+  color: #6f7275;
+  font-size: 20px;
+`;
+export const Location = styled.p`
+  margin: 0;
+  color: #6f7275;
+  font-size: 20px;
+`;
+export const Stats = styled.ul`
+  display: flex;
+  padding: 0;
+  list-style: none;
+  margin: 0;
+  border-top: 1px solid #d5dce6;
+  background-color: #f0f2f5;
+  font-size: 18px;
+`;
+export const StatsItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  width: 33.33%;
+  padding-top: 40px;
+  padding-bottom: 40px;
+  border-right: 1px solid #d5dce6;
+`;
+export const Label = styled.span`
+  color: #6f7275;
+`;
+export const Quantity = styled.span`
+  font-size: 20px;
+  font-weight: bold;
+`;
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,48 @@
+import PropTypes from 'prop-types';
+import {
+  ProfileContainer,
+  Description,
+  Avatar,
+  UserName,
+  Tag,
+  Location,
+  Stats,
+  StatsItem,
+  Label,
+  Quantity,
+} from 'components/Profile.styled';
+
+export const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileContainer>
+      <Description>
+        <Avatar src={avatar} alt="User avatar" />
+        <UserName>{username}</UserName>
+        <Tag>@{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+      <Stats>
+        <StatsItem>
+          <Label>Followers</Label>
+          <Quantity>{stats.followers}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Views</Label>
+          <Quantity>{stats.views}</Quantity>
+        </StatsItem>
+        <StatsItem>
+          <Label>Likes</Label>
+          <Quantity>{stats.likes}</Quantity>
+        </StatsItem>
+      </Stats>
+    </ProfileContainer>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.object.isRequired,
+};
